8e98d6e24c7177b2588791b2f28187690464a6df
ducngo123456 <135568147+duc-ngo-saltovn@users.noreply.github.com>
2024-01-09 15:16:42 +0700

    update quiz folder

--- blockchain/tutorial/workshop01/hardhat.config.js
+++ blockchain/tutorial/workshop01/hardhat.config.js
@@ -1,6 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  networks: {
+    zerachaintest: {
+      url: 'https://rpc.ankr.com/zetachain_evm_athens_testnet',
+      accounts: [process.env.PRIV_KEY]
+    },
+    bsctest: {
+      url: 'https://data-seed-prebsc-1-s1.binance.org:8545/',
+      accounts: [process.env.PRIV_KEY]
+    }
+  }
 };
